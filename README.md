f79967377b4e66864f4d481b92bf44cc7ed79c11
Alex Worden <alexworden@gmail.com>
2023-07-20 15:19:15 -0700

    Ability to control actions from chat loop.

---
+++ README.md
@@ -0,0 +1,8 @@
+#Building
+
+Set the python interpreter in your editor to conda (install it if necessary)
+
+pip install -r requirements.txt
+
+
+
--- genCoverLetter.py
+++ genCoverLetter.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import shutil
 
 import openai
 from langchain.chains import ConversationalRetrievalChain, RetrievalQA
@@ -18,42 +19,25 @@ os.environ["OPENAI_API_KEY"] = constants.APIKEY
 # Enable to save to disk & reuse the model (for repeated queries on the same data)
 PERSONAL_DOCS_FOLDER = "personal_docs"
 PERSIST_FOLDER = PERSONAL_DOCS_FOLDER + "_persist"
+# GPT_MODEL = "gpt-3.5-turbo"
+GPT_MODEL = "gpt-3.5-turbo"
 
-
-# If PERSIST is enabled and the persist directory exists, reuse the index
-if os.path.exists(PERSIST_FOLDER): 
-  print("Reusing vectorstore from " + PERSIST_FOLDER + " directory...\n")
-  vectorstore = Chroma(persist_directory=PERSIST_FOLDER, embedding_function=OpenAIEmbeddings())
-  index = VectorStoreIndexWrapper(vectorstore=vectorstore)
-else:
-  #loader = TextLoader("data/data.txt") # Use this line if you only need data.txt
-  loader = DirectoryLoader(PERSONAL_DOCS_FOLDER)
-  print("\nCreating index in persist directory...\n")
+def rebuild_embedding_chain(persist_folder, personal_docs_folder, gpt_model=GPT_MODEL):
+  print("Rebuilding the personal docs index...")
+  # if the persist_folder exists, delete it
+  if os.path.exists(persist_folder):
+    shutil.rmtree(persist_folder)
+  # create a new index
+  loader = DirectoryLoader(personal_docs_folder)
   index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":PERSIST_FOLDER}).from_loaders([loader])
+  chain = ConversationalRetrievalChain.from_llm(
+    llm=ChatOpenAI(model=gpt_model),
+    retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+  )
+  return chain
 
-
-#Read the contents of job description file into a string
-
-
-chain = ConversationalRetrievalChain.from_llm(
-  llm=ChatOpenAI(model="gpt-3.5-turbo"),
-  retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
-)
-
-chat_history = []
-
-print("Ask questions about your skills and experience from documents in the " + PERSIST_FOLDER + " folder. Enter 'quit' to exit.\n")
-while True:
-  query = input(": ")
-  if query in ['quit', 'q', 'exit']:
-    break
-  result = chain({"question": query, "chat_history": chat_history})
-  print(result['answer'])
-
-  chat_history.append((query, result['answer']))
-  query = None
-
-while True:
+def generate_cover_letter():
+  while True:
     chat_history = []
     company_name = input("Enter the company name: ")
     job_title = input("Enter the job title: ")
@@ -61,11 +45,11 @@ while True:
     print("\nEnter the job description. Enter END on a new line when you are done.")
     lines = []
     while True:
-        line = input()
-        if line == "END":
-            break
-        lines.append(line)
-    
+      line = input()
+      if line == "END":
+        break
+      lines.append(line)
+
     # Join the lines together into a String with a newline character between each line
     job_desc = "\n".join(lines)
 
@@ -85,16 +69,52 @@ while True:
     pdf.multi_cell(0, 5, result['answer'])
     pdf.output(company_name + "_" + job_title + ".pdf")
 
-   #ask the user if they want to generate another cover letter
-   # if they say no, exit the program
-   # if they say yes, repeat the process
-   # if they enter an invalid response, ask them again
+    #ask the user if they want to generate another cover letter
+    # if they say no, exit the program
+    # if they say yes, repeat the process
+    # if they enter an invalid response, ask them again
     while True:
-        answer = input("Generate another cover letter? (y/n): ")
-        if answer == "n":
-            sys.exit()
-        elif answer == "y":
-            break
-        else:
-            print("Invalid response. Please enter y or n.")
+      answer = input("Generate another cover letter? (y/n): ")
+      if answer == "n":
+        return
+      elif answer == "y":
+        break
+      else:
+        print("Invalid response. Please enter y or n.")
+
+
+# ================== START OF MAIN PROGRAM ==================
+# If PERSIST is enabled and the persist directory exists, reuse the index
+if os.path.exists(PERSIST_FOLDER): 
+  print("Reusing vectorstore from " + PERSIST_FOLDER + " directory...\n")
+  vectorstore = Chroma(persist_directory=PERSIST_FOLDER, embedding_function=OpenAIEmbeddings())
+  index = VectorStoreIndexWrapper(vectorstore=vectorstore)
+  chain = ConversationalRetrievalChain.from_llm(
+    llm=ChatOpenAI(model=GPT_MODEL),
+    retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+  )
+else:
+  chain = rebuild_embedding_chain(PERSIST_FOLDER, PERSONAL_DOCS_FOLDER)
+
+chat_history = []
+
+print("Ask questions about your skills and experience from documents in the " + PERSIST_FOLDER + " folder. Enter 'quit' to exit.\n")
+while True:
+  query = input(": ")
+  if query in ['quit', 'q', 'exit']:
+    break
+  elif query == "Gen Cover Letter":
+    generate_cover_letter()
+  elif query == "Refresh":
+    chain = rebuild_embedding_chain(PERSIST_FOLDER, PERSONAL_DOCS_FOLDER)
+  else:
+    result = chain({"question": query, "chat_history": chat_history})
+    print(result['answer'])
+
+    chat_history.append((query, result['answer']))
+    query = None
+
+# BEGIN: 9f8c6549bwf9
+
+# END: 9f8c6549bwf9
     
